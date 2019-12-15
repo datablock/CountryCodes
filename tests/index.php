9a0f72e35264dde5b79f552e955b9a60a9eb2c6d
Arnaud BODEL <contact@osw3.net>
2019-12-15 13:41:17 +0100

    -

--- tests/index.php
+++ tests/index.php
@@ -3,80 +3,83 @@ require_once "../vendor/autoload.php";
 
 use \Datablock\CountryCodes\CountryCodes;
 
-$countrycodes = new CountryCodes();
+// Make instance
+// --
+// $countrycodes = new CountryCodes();
+// $countrycodes = new CountryCodes('FR');
 
-// // Set country
-// // --
+// Set country
+// --
 // $countrycodes->setCountry('FR');
 
-// // Get database
-// // --
+// Get database
+// --
 // print_r( $countrycodes->getDatabase() );
 
-// // Get database as jSon
-// // --
+// Get database as jSon
+// --
 // print_r( $countrycodes->getJsonDatabase() );
 
-// // Get Country Name
-// // --
+// Get Country Name
+// --
 // print_r($countrycodes->getCountryName())."\n";
 
-// // Get ISO 2
-// // --
+// Get ISO 2
+// --
 // print_r($countrycodes->getIso2())."\n";
 
-// // Get ISO 3
-// // --
+// Get ISO 3
+// --
 // print_r($countrycodes->getIso3())."\n";
 
-// // Get TLD
-// // --
+// Get TLD
+// --
 // print_r($countrycodes->getTld())."\n";
 
-// // Get FIPS
-// // --
+// Get FIPS
+// --
 // print_r($countrycodes->getFips())."\n";
 
-// // Get ISO Numeric
-// // --
+// Get ISO Numeric
+// --
 // print_r($countrycodes->getIsoNumeric())."\n";
 
-// // Get Geoname ID
-// // --
+// Get Geoname ID
+// --
 // print_r($countrycodes->getGeonameId())."\n";
 
-// // Get E164
-// // --
+// Get E164
+// --
 // print_r($countrycodes->getE164())."\n";
 
-// // Get Phone Code
-// // --
+// Get Phone Code
+// --
 // print_r($countrycodes->getPhoneCode())."\n";
 
-// // Get Continent
-// // --
+// Get Continent
+// --
 // print_r($countrycodes->getContinent())."\n";
 
-// // Get Capital
-// // --
+// Get Capital
+// --
 // print_r($countrycodes->getCapital())."\n";
 
-// // Get Timezone
-// // --
+// Get Timezone
+// --
 // print_r($countrycodes->getTimezone())."\n";
 
-// // Get Currency
-// // --
+// Get Currency
+// --
 // print_r($countrycodes->getCurrency())."\n";
 
-// // Get Language Codes
-// // --
+// Get Language Codes
+// --
 // print_r($countrycodes->getLanguageCodes())."\n";
 
-// // Get Languages
-// // --
+// Get Languages
+// --
 // print_r($countrycodes->getLanguages())."\n";
 
-// // Get Area
-// // --
+// Get Area
+// --
 // print_r($countrycodes->getArea())."\n";
